68e87eca1505802b6ad21457be93ddef4f8214f5
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2021-12-20 00:54:03 -0500

    crear script moods.js

---
+++ scripts/moods.js
@@ -0,0 +1,17 @@
+var magali=document.getElementById("magali");
+magali.addEventListener("click", moodMagali);
+var tora=document.getElementById("tora");
+tora.addEventListener("click", moodTora);
+var wanda=document.getElementById("wanda");
+wanda.addEventListener("click", moodWanda);
+
+function moodMagali() {
+    alert("No me que toques!");
+}
+function moodTora() {
+    alert("Más caricias por favor");
+}
+function moodWanda() {
+    alert("...te veo...");
+}
+        
